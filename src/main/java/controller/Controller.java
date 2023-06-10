ab0bfb9faf8f9449dcfb642b03483ca58eb95b54
Robson Ferreira <robson_20ferreira@hotmail.com>
2023-06-10 18:58:24 -0300

    Atualizacao do Controller e JavaBeans

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,17 +1,21 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.DAO;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+import model.DAO;
+import model.JavaBeans;
+
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans cadastro = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -23,14 +27,37 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			cadastro(request, response);
+		} else if (action.equals("/insert")) {
+			novoCadastro(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
-
 	}
 
 	// listar cadastro
 	protected void cadastro(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("cadastro.jsp");
+	}
+
+	// Novo cadastro
+	protected void novoCadastro(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste de recebimento dos dados do formulário
+		/**
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("quant"));
+		System.out.println(request.getParameter("tomb"));
+		System.out.println(request.getParameter("infor"));
+		System.out.println(request.getParameter("valor"));
+		**/
+		
+		// setar as variaveis JavaBeans
+		cadastro.setNome(request.getParameter("nome"));
+		cadastro.setQuant(request.getParameter("quant"));
+		cadastro.setTomb(request.getParameter("tomb"));
+		cadastro.setText(request.getParameter("infor"));
+		cadastro.setValor(request.getParameter("valor"));
 	}
 
 }
--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -1,62 +1,74 @@
 package model;
 
 public class JavaBeans {
-private String idDF;
-private String nome;
-private Integer quant;
-private String tomb;
-private String text;
-private Double valor;
+	private String idDF;
+	private String nome;
+	private String quant;
+	private String tomb;
+	private String text;
+	private String valor;
 
-public JavaBeans() {
-	super();
-	
-}
+	public JavaBeans() {
+		super();
 
-public JavaBeans(String idDF, String nome, Integer quant, String tomb, String text, Double valor) {
-	super();
-	this.idDF = idDF;
-	this.nome = nome;
-	this.quant = quant;
-	this.tomb = tomb;
-	this.text = text;
-	this.valor = valor;
-}
+	}
 
-public String getIdDF() {
-	return idDF;
-}
-public void setIdDF(String idDF) {
-	this.idDF = idDF;
-}
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public Integer getQuant() {
-	return quant;
-}
-public void setQuant(Integer quant) {
-	this.quant = quant;
-}
-public String getTomb() {
-	return tomb;
-}
-public void setTomb(String tomb) {
-	this.tomb = tomb;
-}
-public String getText() {
-	return text;
-}
-public void setText(String text) {
-	this.text = text;
-}
-public Double getValor() {
-	return valor;
-}
-public void setValor(Double valor) {
-	this.valor = valor;
-}
+	public JavaBeans(String idDF, String nome, String quant, String tomb, String text, String valor) {
+		super();
+		this.idDF = idDF;
+		this.nome = nome;
+		this.quant = quant;
+		this.tomb = tomb;
+		this.text = text;
+		this.valor = valor;
+	}
+
+	public String getIdDF() {
+		return idDF;
+	}
+
+	public void setIdDF(String idDF) {
+		this.idDF = idDF;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getQuant() {
+		return quant;
+	}
+
+	public void setQuant(String quant) {
+		this.quant = quant;
+	}
+
+	public String getTomb() {
+		return tomb;
+	}
+
+	public void setTomb(String tomb) {
+		this.tomb = tomb;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
 }
